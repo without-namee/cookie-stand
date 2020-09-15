728211342ee619622a2f9ef4a58f12a079916be0
Dana Kiswani <danakiswani0@gmail.com>
2020-09-15 04:09:13 +0300

    refactor my code

--- JS/app.js
+++ JS/app.js
@@ -1,69 +1,131 @@
-
 'use strict';
 
 var hours = ['6:00 am', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm'];
-var arr=[];
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+var divTable = document.getElementById('divTable');
 
+var table = document.createElement('table');
+divTable.appendChild(table);
 
+var totalArray = [];
 
-function Dnew(location,min,max,avgCookies){
-this.location= location;
-this.minCustomers=min;
-this.maxCustomers=max;
-this.avgCookie=avgCookies;
-this.cookiezArray=[];
-this.totalCookie=0;
-arr.push(this);
-}
+//-------------------------------------------
 
-Dnew.prototype.randomCustm=function() {
-  for( var i=0; i < hours.length; i++){
-    var a=Math.floor( getRandomInt(this.minCustomers, this.maxCustomers)*this.avgCookie);
-    this.cookiezArray.push (a);
-    console.log(a);
-  }
+function SalmonStore(location , minCustomers , maxCustomers, avgCookie ){
+
+this.location = location;
+this.minCustomers = minCustomers;
+this.maxCustomers = maxCustomers;
+this.avgCookie = avgCookie;
+this.custArray = [];
+this.totalCookie = 0;
+totalArray.push(this);
+
+this.randomNumber();
 };
+//-------------------------------------------
 
-Dnew.prototype.rendre=function(){
-var divx = document.getElementById('divDoc');
-
-var seatlleH2 = document.createElement('h2');
-seatlleH2.textContent=this.location;
-divx.appendChild(seatlleH2);
-
-
-var ul1 = document.createElement('ul');
-divx.appendChild(ul1);
-
-
-for (var x = 0; x < hours.length; x++){
-  this.totalCookie+=this.cookiezArray[x];
-  var lix = document.createElement('li');
-  lix.textContent = hours[x] + ' ' + this.cookiezArray[x]+' cookies';
-  ul1.appendChild(lix);
-
-}
-var liTotal = document.createElement('li');
-liTotal.textContent = ' Total: '+ this.totalCookie + ' cookies';
-ul1.appendChild(liTotal);
+SalmonStore.prototype.randomNumber = function(){
+    for( var i=0; i < hours.length; i++){
+       this.custArray.push(Math.floor(getRandomInt(this.minCustomers, this.maxCustomers) * this.avgCookie));
+      }
 }
 
-new Dnew('Lima',2,16,4.6);
-new Dnew('Lima',2,16,4.6);
-new Dnew('Lima',2,16,4.6);
-new Dnew('Lima',2,16,4.6);
-new Dnew('Lima',2,16,4.6);
+//--------------------------------------------
+function tableRender (){
 
-for(var a=0;a<arr.length;a++){
-arr[a].randomCustm();
-arr[a].rendre();
+    var trMaker = document.createElement('tr');
+    table.appendChild(trMaker);
+
+    var tdName = document.createElement('td');
+    tdName.textContent = 'Table ';
+    trMaker.appendChild(tdName);
+
+    for (var x = 0; x < hours.length; x++){
+    var tdMaker = document.createElement('td');
+    tdMaker.textContent = hours[x] ;
+    trMaker.appendChild(tdMaker);
+
+
+
+    }
+  }
+
+  //-------------------------------------------
+
+SalmonStore.prototype.countries = function(){
+
+    var trMaker = document.createElement('tr');
+    table.appendChild(trMaker);
+
+    var tdName = document.createElement('td');
+    tdName.textContent = this.location;
+    trMaker.appendChild(tdName);
+
+    for (var x = 0; x < hours.length; x++){
+        var tdMaker = document.createElement('td');
+        tdMaker.textContent = this.custArray[x] ;
+        trMaker.appendChild(tdMaker);
+
+    }
+
 }
 
-// //--------------------------------------------------
+
+  //-------------------------------------------
+
+  
+  new SalmonStore('Seatlle', 23, 65, 6.3);
+  new SalmonStore('Tokyo', 3, 24, 1.2);
+  new SalmonStore('Dubai', 11, 38, 3.7);
+  new SalmonStore('Paris', 20, 38, 2.3);
+  new SalmonStore('Lima', 2, 16, 4.6);
+
+
+
+  //-------------------------------------------
+
+  tableRender();
+
+//   stores();
+  for (var index = 0; index < totalArray.length; index++) {
+
+      totalArray[index].countries();
+    }
+
+
+
+
+
+
+
+
+
+
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
