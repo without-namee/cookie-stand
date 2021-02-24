42e65bb3a0540575fa0366b1d4f472ef970f19be
Dana Kiswani <70090810+Dana-Kiswani@users.noreply.github.com>
2021-02-24 11:12:01 +0200

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,6 @@
 'use strict';
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
 // -----------------------------------------------
 var container = document.getElementById('divTable');
 var tableEle = document.createElement('table');
@@ -17,6 +18,8 @@ function Shop(name, minCust, maxCust, avgCoookies) {
   dStore.push(this);
   this.cookies();
 }
+
+// -----------------------------------------------
 Shop.prototype.cookies = function () {
   for (var i = 0; i < hours.length; i++) {
     var calcuPerHour = Math.floor(getRandomNum(this.maxCust, this.minCust) * this.avgCoookies);
@@ -24,6 +27,7 @@ Shop.prototype.cookies = function () {
     this.cookiesTotal += this.cookiesArr[i];
   }
 };
+
 // -----------------------------------------------
 function header() {
   var trX = document.createElement('tr');
@@ -40,6 +44,7 @@ function header() {
   thTotal.textContent = 'Total';
   trX.appendChild(thTotal);
 }
+
 // -----------------------------------------------
 Shop.prototype.render = function(){
   var trX =document.createElement('tr');
@@ -56,14 +61,15 @@ Shop.prototype.render = function(){
   trX.appendChild(tdCookieTotal);
   tdCookieTotal.textContent = this.cookiesTotal;
 };
+
 // -----------------------------------------------
 new Shop('Seattle', 23, 65, 6.3);
 new Shop('Tokyo', 3, 24, 1.2);
 new Shop('Dubai', 11, 38, 3.7);
 new Shop('Paris', 20, 38, 2.3);
 new Shop('Lima', 2, 16, 4.6);
-// -----------------------------------------------
 
+// -----------------------------------------------
 function footer() {
   var trX = document.createElement('tr');
   tableEle.appendChild(trX);
@@ -91,6 +97,7 @@ for (var i = 0; i <dStore.length;i++){
   dStore[i].render();
 }
 footer();
+
 // -----------------------------------------------
 
 var locationForm = document.getElementById('OrderX');
@@ -106,11 +113,13 @@ locationForm.addEventListener('submit', function (event){
   newStore.render();
   footer();
 });
+
 // -----------------------------------------------
 function getRandomNum(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 // -----------------------------------------------
 
